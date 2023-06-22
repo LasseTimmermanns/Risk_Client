624f8c0e620c4e0eef01cfea3440c2466ec89d73
LasseTimmermanns <lasse.mainkonto@gmail.com>
2023-06-22 15:24:47 +0200

    Made SettingsDisplay Scrollable

--- src/app/Components/lobby/settings-display/settings-display.component.ts
+++ src/app/Components/lobby/settings-display/settings-display.component.ts
@@ -1,18 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, SimpleChanges, ViewChild } from '@angular/core';
 import { Lobby } from 'src/app/Services/Lobby/Lobby';
-import { QueryIdentification } from '../QueryIdentification';
-import { LobbyService } from 'src/app/Services/Lobby/lobby.service';
-import { InputEvent } from '../../shared/InputEvent';
 
 @Component({
   selector: 'app-settings-display',
   templateUrl: './settings-display.component.html',
-  styleUrls: ['./settings-display.component.scss']
+  styleUrls: ['./settings-display.component.scss', './settings-display-scroll.scss']
 })
 export class SettingsDisplayComponent {
 
 
   @Input() lobby?: Lobby;
+  @ViewChild("wrapper") wrapper?: ElementRef<HTMLDivElement>;
 
+  scrollable: boolean = false;
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.updateScrollability()
+  }
+
+  @HostListener("window:resize")
+  onResize(){
+    this.updateScrollability();
+  }
+
+  scroll(multiplier: number){
+    const max_scroll_width = this.wrapper!.nativeElement.offsetWidth / 2.5;
+    this.wrapper!.nativeElement.scrollLeft += multiplier * max_scroll_width;
+  }
+
+  updateScrollability(){
+    if(!this.wrapper) return;
+    this.scrollable = this.wrapper.nativeElement.scrollWidth > this.wrapper.nativeElement.offsetWidth;
+  }
 }
